4126cb8cdaedbd7e1ec0f4b143fd7c7a26e64dbc
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@hotmail.com>
2017-12-29 12:25:42 +0100

    Adaptative layouts has been added

--- Paperboy/Paperboy/Views/TestingPage.xaml.cs
+++ Paperboy/Paperboy/Views/TestingPage.xaml.cs
@@ -16,5 +16,20 @@ namespace Paperboy.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width > height)
+            {
+                outerStack.Orientation = StackOrientation.Horizontal;
+            }
+            else
+            {
+                outerStack.Orientation = StackOrientation.Vertical;
+            }
+
+        }
+    }
 }
